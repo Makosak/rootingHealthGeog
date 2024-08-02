2b0e126d61e4274764db0c75d6426703b69acf69
Marynia <marynia.kolak@gmail.com>
2024-08-01 22:38:21 -0400

    Update figs and readme

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
 # rootingHealthGeog
- pubmed mini-analysis of conceptual themes & geography terms
+Pubmed mini-analysis of conceptual themes & geography terms
+ 
+Data can be downloaded directly from [PubMed](https://pubmed.ncbi.nlm.nih.gov/). 
 
 ## Indexed PubMed Studies By Year & Conceptual Framework
 
-![Figure 1](PubMed_Citations_Framework.png)
+![Figure 1](Fig1_PubMedConcept.png)
 
-## Indexed PubMed Studies By Year & Geography Concept
-
-![Figure 2](Concept_IndexedPub.png)
-
-## Geography Concepts in PubMed Publications since 1800
+```
+ggplot(main.df2,aes(x=year, fill=category, group=category)) + 
+  geom_histogram(position='stack', stat="count") + 
+  labs(fill = "Framework", x = "Year of Publication", y = "Category Count") +
+  scale_fill_hue(labels = c("Geography","Health Geography","Medical Geography","SDOH")) +
+  scale_x_date(limit=c(as.Date("1799-01-01"),as.Date("2023-12-30"))) +
+  theme_tufte() 
+```
+## Geography Concepts in PubMed Publications since 1799.
 The count of concepts is included for each year. 
 
-![Figure 3](countCategory_PubYear.png)
+![Figure 3](Fig2_PubMedFrameworkCnt.png)
+```
+ggplot(main.df2,aes(x=year,y=total,colour=category)) + 
+  geom_line() + 
+  labs(color = "Concept", x = "Year of Publication", y = "Indexed Publications") + 
+  scale_color_hue(labels = c("Geography","Human Geography",
+                             "Medical Geography", "SDOH")) +
+  scale_x_date(limit=c(as.Date("1799-01-01"),as.Date("2023-12-30"))) +
+  theme_tufte() 
+```
+
+## Indexed PubMed Studies By Year & SDOH Concept 
+
+![Figure 2](Fig3_PubMedSDOHConcept.png)
+
+```
+ggplot(main.df1,aes(x=yrs,y=total,colour=category)) + 
+  geom_line() + 
+  labs(color = "Concept", x = "Year of Publication", y = "Indexed Publications") + 
+  theme_tufte() +
+  scale_color_hue(labels = c("Socioecological", "Exposome","Fundamentals","Intersectionality",
+                             "Political Ecology","Risk Environment","Salutogenesis")) +
+  scale_x_date(limit=c(as.Date("1980-01-01"),as.Date("2023-12-30"))) 
+```
 
--- code.R
+++ code.R
@@ -1,12 +1,13 @@
 setwd("~/code/rootingHealthGeog")
 library(tidyverse)
 library(lubridate)
+library(ggthemes)
 
 
 #### #### #### 
 #### SDOH #### 
 #### #### #### 
-sdoh <- read.csv("sdoh.csv")
+sdoh <- read.csv("data_raw/sdoh.csv")
 
 sdoh$yrs <- lubridate::ymd(sdoh$Year, truncated = 2L)
 sdoh$category <- "sdoh"
@@ -18,7 +19,7 @@ df <- sdoh %>%
 glimpse(df) #61 SDOH
 
 #### #### #### 
-riskEnv <- read.csv("riskEnv.csv")
+riskEnv <- read.csv("data_raw/riskEnv.csv")
 riskEnv$yrs <- lubridate::ymd(riskEnv$Year, truncated = 2L)
 riskEnv$category <- "riskEnv"
 
@@ -33,7 +34,7 @@ main.df <- rbind(df,riskEnv1)
 glimpse(main.df) #101
 
 #### #### #### 
-Int <- read.csv("intersectionality.csv")
+Int <- read.csv("data_raw/intersectionality.csv")
 Int$yrs <- lubridate::ymd(Int$Year, truncated = 2L)
 Int$category <- "intSct"
 
@@ -48,7 +49,7 @@ glimpse(main.df) #125
 
 
 #### #### #### 
-Exp <- read.csv("exposome.csv")
+Exp <- read.csv("data_raw/exposome.csv")
 Exp$yrs <- lubridate::ymd(Exp$Year, truncated = 2L)
 Exp$category <- "Exps"
 
@@ -62,7 +63,7 @@ main.df <- rbind(main.df,Exp)
 glimpse(main.df) #142
 
 #### #### #### 
-Fun <- read.csv("fundamental.csv")
+Fun <- read.csv("data_raw/fundamental.csv")
 Fun$yrs <- lubridate::ymd(Fun$Year, truncated = 2L)
 Fun$category <- "funDm"
 
@@ -76,7 +77,7 @@ main.df <- rbind(main.df,Fun)
 glimpse(main.df) #183
 
 #### #### #### 
-PolE <- read.csv("politicalEcology.csv")
+PolE <- read.csv("data_raw/politicalEcology.csv")
 PolE$yrs <- lubridate::ymd(PolE$Year, truncated = 2L)
 PolE$category <- "poleco"
 
@@ -90,7 +91,7 @@ main.df <- rbind(main.df,PolE)
 glimpse(main.df) #237
 
 #### #### #### 
-Salut <- read.csv("salutogenesis.csv")
+Salut <- read.csv("data_raw/salutogenesis.csv")
 Salut$yrs <- lubridate::ymd(Salut$Year, truncated = 2L)
 Salut$category <- "salut"
 
@@ -103,53 +104,83 @@ glimpse(Salut) #45 Int
 main.df <- rbind(main.df,Salut)
 glimpse(main.df) #282
 
+#### #### #### 
+Eco <- read.csv("data_raw/socioEcological.csv")
+Eco$yrs <- lubridate::ymd(Eco$Year, truncated = 2L)
+Eco$category <- "eco"
+
+Eco <- Eco %>%
+  select(yrs, Count, category) %>%
+  rename(total = Count)
+
+glimpse(Eco) #45 Int
+
+main.df <- rbind(main.df,Eco)
+glimpse(main.df) #327
+
 ##################
 ##################
 
 ##write.csv(main, "wide-format.csv")
 
-write.csv(main.df, "long-format.csv")
+write.csv(main.df, "long-format2.csv")
 ##################
 ##################
 
+head(main.df)
+unique(main.df$category)
 
-p <- ggplot(main, aes(x=yrs)) + 
-  geom_line(aes(y = riskEnv), color="steelblue", linetype="twodash") +
-  geom_line(aes(y = Exposome), color="green", linetype="twodash") +
-  geom_line(aes(y = FundM), color="purple", linetype="twodash") +
-  geom_line(aes(y = PolEco), color="black", linetype="twodash") +
-  geom_line(aes(y = Salut), color="orange", linetype="twodash") +
-  geom_line(aes(y = Intersectionality), color="red", linetype="twodash")  
+# This doesn't work anymore
+ggplot(main.df, aes(x=yrs,y=total,colour=category)) + 
+  geom_line(aes(y = riskEnv), color="steelblue", linetype="twodash") 
 
-p+scale_x_date(limit=c(as.Date("1999-01-01"),as.Date("2023-12-30")))
+  geom_line(aes(y = Exps), color="green", linetype="twodash") +
+  geom_line(aes(y = funDm), color="purple", linetype="twodash") +
+  geom_line(aes(y = poleco), color="black", linetype="twodash") +
+  geom_line(aes(y = salut), color="orange", linetype="twodash") +
+  geom_line(aes(y = intSct), color="red", linetype="twodash") 
+
+  scale_x_date(limit=c(as.Date("1999-01-01"),as.Date("2023-12-30")))
 
 
 ################
 
-main.df = read.csv("long-format.csv")
+main.df = read.csv("long-format2.csv")
+glimpse(main.df)
 
 main.df1 = filter(main.df, main.df$category != "sdoh")
 
 head(main.df1)
 
+main.df1$yrs <- lubridate::ymd(main.df1$yrs, truncated = 2L)
+glimpse(main.df1)
+
+
+## This one works!!
 ggplot(main.df1,aes(x=yrs,y=total,colour=category)) + 
   geom_line() + 
-  labs(color = "Framework", x = "Year of Publication", y = "Indexed Publications") +
-  scale_color_hue(labels = c("Exposome","Fundamentals","Intersectionality",
-                                "Political Ecology","Risk Environment","Salutogenesis")) +
-  theme_tufte() 
+  labs(color = "Concept", x = "Year of Publication", y = "Indexed Publications") + 
+  theme_tufte() +
+  scale_color_hue(labels = c("Socioecological", "Exposome","Fundamentals","Intersectionality",
+                             "Political Ecology","Risk Environment","Salutogenesis")) +
+  scale_x_date(limit=c(as.Date("1980-01-01"),as.Date("2023-12-30"))) 
+  
   #scale_color_brewer(palette="Set3") + 
-  #scale_x_tufte() + scale_y_tufte()
 
+#   
 
 ################
 
-glimpse(sdoh)
+main.df = read.csv("long-format2.csv")
+glimpse(main.df)
+
+main.df$yrs <- lubridate::ymd(main.df$yrs, truncated = 2L)
+glimpse(main.df)
+
 sdoh = filter(main.df, main.df$category != "sdoh" |
                 main.df$category != "geog" | 
                 main.df$category != "Mgeog"| 
                 main.df$category != "Hgeog")
-
 
 #### #### #### 
 geog <- read.csv("data_raw/geography.csv")
@@ -181,7 +212,6 @@ write.csv(main.df, "long-format1.csv")
 ###############################
 
 main.df = read.csv("long-format1.csv")
-library(ggthemes)
 
 glimpse(main.df)
 summary(main.df)
@@ -197,19 +227,24 @@ glimpse(main.df2) #306
 ggplot(main.df2,aes(x=year, y=total, fill=category, group=category)) + 
   geom_bar(stat = 'identity',position='stack') + 
   labs(color = "Framework", x = "Year of Publication", y = "PubMed Citations") +
+  scale_x_date(limit=c(as.Date("1799-01-01"),as.Date("2023-12-30"))) +
   theme_tufte() 
 
+## Line graph of Geog/Health Geog/Medical Geog/SDOOH
 ggplot(main.df2,aes(x=year,y=total,colour=category)) + 
   geom_line() + 
   labs(color = "Concept", x = "Year of Publication", y = "Indexed Publications") + 
   scale_color_hue(labels = c("Geography","Human Geography",
                              "Medical Geography", "SDOH")) +
+  scale_x_date(limit=c(as.Date("1799-01-01"),as.Date("2023-12-30"))) +
   theme_tufte() 
 
+## Category Count Visual of Geog/Health Geog/Medical Geog/SDOOH
 ggplot(main.df2,aes(x=year, fill=category, group=category)) + 
   geom_histogram(position='stack', stat="count") + 
   labs(fill = "Framework", x = "Year of Publication", y = "Category Count") +
   scale_fill_hue(labels = c("Geography","Health Geography","Medical Geography","SDOH")) +
+  scale_x_date(limit=c(as.Date("1799-01-01"),as.Date("2023-12-30"))) +
   theme_tufte() 
 
 filter(main.df2, year == 1800) 
